85a2b32090874a750501e5dcc04e5406a7b1ed8c
abouthiroppy <git@about-hiroppy.com>
2015-12-22 05:56:21 +0900

    Implement last commit sha at Github

--- app/js/components/Dashboard.js
+++ app/js/components/Dashboard.js
@@ -10,21 +10,48 @@ import Travis from './dashboard/Travis';
 export default class extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      sha: '',
+      uri: ''
+    };
+
+    this._fetchLastCommit();
   }
 
   _initCss() {
     return {
+      lastCommitLabel: {
+        marginTop: '15px'
+      },
       travisBadge: {
         verticalAlign: 'middle'
       }
     };
   }
 
+  _fetchLastCommit() {
+    const uri        = 'https://api.github.com/repos/abouthiroppy/mattya/git/refs/heads/master';
+    const repoPrefix = 'https://github.com/abouthiroppy/mattya/commit/';
+
+    $.get(uri, (res) => {
+      this.setState({sha: res.object.sha});
+      this.setState({uri: `${repoPrefix}${res.object.sha}`});
+    });
+  }
+
   render() {
     return (
       <div>
-      <div className="card-panel teal lighten-2">Teach last version at git
-        <img style={this._initCss().travisBadge} className="right"
+      <div className="card-panel teal lighten-1" style={this._initCss().lastCommitLabel}>
+        <span className="white-text text-darken-4">
+          <i className="fa fa-github-alt"></i>
+          &nbsp;Latest Commit on Github:&nbsp;
+          <a className="white-text text-darken-4" href={this.state.uri} target="_blank">
+            {this.state.sha}
+          </a>
+        </span>
+        <img className="right" style={this._initCss().travisBadge}
           src="https://travis-ci.org/abouthiroppy/mattya.svg?branch=master" />
       </div>
         <Menu />
